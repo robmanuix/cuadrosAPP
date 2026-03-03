048dd7639f4fd8d85bd7c76f4aa8832b24260991
Roberto Mantilla <mantillaraujo91@hotmail.com>
2026-03-02 21:33:41 -0500

    actualización de primer commit para cuadrosApp

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/cuadros-app/',
+  base: '/cuadrosAPP/',
   plugins: [
     react(),
     tailwindcss(),
